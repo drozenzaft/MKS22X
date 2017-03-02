50f7a29f81756b1addad6b382aace2440efc1314
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-02 14:49:22 -0500

    More tests

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -2,11 +2,12 @@ public class Driver{
 
     public static void main(String[]args){
         Maze f;
-        f = new Maze("data3.dat");//true animates the maze.
-        
-//        f.setAnimate(true);
-        f.solve();
-
-        System.out.println(f);
+        for (int i = 0; i < 6; i++) {
+	    f = new Maze("data" + i + ".dat");//true animates the maze.
+		//        f.setAnimate(true);
+	    f.solve();
+	    System.out.println("Maze #" + i + ":");
+	    System.out.println(f); 
+	}
     }
 }
