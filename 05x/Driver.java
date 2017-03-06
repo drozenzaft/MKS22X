7541b5641014ebce16755769ef08eac44ce0a4d6
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-06 14:10:54 -0500

    Renaming this

---
+++ 05x/Driver.java
@@ -0,0 +1,11 @@
+public class Driver{
+
+    public static void main(String[]args){
+        Travel t;
+        t = new Travel("ctravel.in");
+		//        f.setAnimate(true);
+        t.solve();
+        System.out.println(t); 
+	}
+    }
+}
