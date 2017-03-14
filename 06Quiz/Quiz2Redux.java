8721437223e2aa13543fcbd4c89f5b7f2adb085f
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-14 03:09:55 -0400

    Completed lab

--- 06Quiz/Quiz2Redux.java
+++ 06Quiz/Quiz2Redux.java
@@ -12,20 +12,42 @@ public class Quiz2Redux{
       return words;
   }
   
-  private static boolean help(ArrayList<String> words, String s, String str, int i) {
-    if (i > s.length()) return true;
-    if (!words.contains(str)) {
-      words.add(str);
-      if (i < s.length() && (help(words,s,str+s.charAt(i),i+1) || help(words,s,""+s.charAt(i),i+1))) return true;
+    private static void help(ArrayList<String> words, String s, String str, int ind) {
+	for (int i = ind; i <= s.length(); i++) {
+	    unique(words,str);
+	    try {
+		help(words,s,str+s.charAt(i),i+1);
+		help(words,s,""+s.charAt(i),i+1);
+	    }
+	    catch (IndexOutOfBoundsException e) {
+		unique(words,str);
+	    }
+	}
+   	return;
     }
-    return false;
-  }
 
-    public static void main(String[] args) {
+    private static void unique(ArrayList<String> ary, String in) {
+	int chance = 0;
+	for (String a : ary) {
+	    if (a.equals(in)) {
+		chance++;
+	    }
+	}
+	//System.out.println(Arrays.toString(ary.toArray(new String[ary.size()]))+","+in+","+chance);
+	if (chance == 0) ary.add(in);
+    }
+    
+    /*private static void help(ArrayList<String> words, String s, String str) {
+	if (words.size() == (int)(Math.pow(2,s.length))) return;
+	for (int i = 0; i < s.length(); i++) {
+	    if (!words.contains(str)) words.add(str);
+	    */
+
+    /*public static void main(String[] args) {
 	Quiz2Redux q = new Quiz2Redux();
 	ArrayList<String> ary = q.combinations(args[0]);
 	String[] c = ary.toArray(new String[ary.size()]);
 	System.out.println(Arrays.toString(c));
-    }
+	}*/
     
 }
