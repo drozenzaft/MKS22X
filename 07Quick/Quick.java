4ae6357a0a789ca543f294a6f005ffdbe618787e
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-16 22:19:20 -0400

    Almost have a perfect partition method, but it's off by one sometimes... yeah i don't know

---
+++ 07Quick/Quick.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Quick {
+    //http://me.dt.in.th/page/Quicksort/
+    //this really helped me understand the algorithm! ^^^
+    private static void swap(int[] data, int ind1, int ind2) {
+	int temp = data[ind2];
+	data[ind2] = data[ind1];
+	data[ind1] = temp;
+    }
+    
+    public static int part(int[] data, int start, int end) {
+	int ind = start + (int)(Math.random()*(end-start));
+	//int ind = 0;
+	int pivot = data[ind];
+	System.out.println("pivot: " +ind+","+pivot);
+	int first = start;
+        int last = 0;
+	for (int i = start; i <= end; i++) {
+	    if (data[i] < pivot) {
+		swap(data,first,i);
+		last = first;
+		first++;
+	    }
+	    System.out.println("first, i: " +last+","+i);
+	    System.out.println(Arrays.toString(data));
+	}
+	swap(data,ind,last);
+	//swap(data,last,last-1);
+	return last;
+    }
+
+    public static void main(String[] args) {
+	Quick a = new Quick();
+	int[] test1 = {3,1,4,1,5,9,2,6,5,3};
+	int[] test = {44,75,23,43,55,12,64,77,33};
+	System.out.println(Arrays.toString(test1));
+	System.out.println(part(test1,0,test1.length-1));
+	System.out.println(Arrays.toString(test1));
+    }
+
+    //-Choose a random element to be a pivot, and partition the array around it. 
+    //-Only partition the elements from start to end inclusive.
+    //-When done returns the index of the final position of the pivot element.      
+    //    (Should be from start to end inclusive)
+}
