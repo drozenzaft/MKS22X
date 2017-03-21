40d36f7d265ab494282f3cf8c73d09d9019d716d
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-21 14:47:39 -0400

    Slightly adjusting these test cases; thanks to Khinshan for the initial file

--- 07Quick/Driver.java
+++ 07Quick/Driver.java
@@ -13,13 +13,13 @@ public class Driver{
     
     public static void main(String [] args){
 	//Partition Test Cases (Need to check the array visually)
-	int []data= randomArray(10, 100);
+	/*int []data= randomArray(10, 100);
 	  System.out.println(Arrays.toString(data));
 	  System.out.println(Quick.part(data, 0, data.length-1));
 	  System.out.println(Arrays.toString(data));
-	
+	*/
 	//K's Test Cases
-	/*
+	
 	  int[]ary = { 2, 10, 15, 23, 0,  5};
 	  System.out.println(Quick.quickselect( ary , 0 )); //would return 0
 	  System.out.println(Quick.quickselect( ary , 1 )); //would return 2
@@ -27,10 +27,10 @@ public class Driver{
 	  System.out.println(Quick.quickselect( ary , 3 )); //would return 10
 	  System.out.println(Quick.quickselect( ary , 4 )); //would return 15
 	  System.out.println(Quick.quickselect( ary , 5 )); //would return 23
-	*/
+	
 	//Quick Select Test Cases, can just look to see if a line isn't spaced and is "ERROR"
 	//Might modify to give more insight, but what works works.
-	  /*	for(int i=0; i<10; i++){
+	  /* 	for(int i=0; i<10; i++){
 	    //checking for a friend; 10 min and 20 max
 	    int size=10+(int)Math.floor(Math.random()*(20-10+1));
 	    int []data= randomArray(size, 1000);
