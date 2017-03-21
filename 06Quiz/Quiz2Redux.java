f8e255378c0ab92eb4b1e63edf6d664f13be3cfe
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-21 14:32:26 -0400

    Quick code cleanup

--- 06Quiz/Quiz2Redux.java
+++ 06Quiz/Quiz2Redux.java
@@ -1,10 +1,5 @@
 import java.util.*;
 public class Quiz2Redux{  
-    /*Returns an ArrayList<String> that contains all subsets of the
-     *characters of String s. Assume s has no duplicate characters.
-     *the characters should appear in the same order that they occur 
-     *in the original string.
-     */
     public static ArrayList<String> combinations(String s){
 	ArrayList<String>words = new ArrayList<String>();
 	help(words,s,"",0);
