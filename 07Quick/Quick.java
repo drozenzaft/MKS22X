1b54b0009f3ed312a44d9b6d5a4a9bda007475f0
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-03-23 00:36:45 -0400

    Completed lab!

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -8,25 +8,28 @@ public class Quick {
 	data[ind1] = temp;
     }
     
-    /*public static int part(int[] data, int start, int end) {
+    /*    public static int part(int[] data, int start, int end) {
 	int ind = start + (int)(Math.random()*(end-start));
 	int pivot = data[ind];
-	System.out.println("pivot: " +ind+","+pivot);
+	//System.out.println("pivot: " +ind+","+pivot);
 	int j = end;
-	int i = 0;
-	swap(data,0,ind);
-	ind = 0;
-	System.out.println("after initial swap: "+ Arrays.toString(data));
+	int i = start+1;
+	swap(data,start,ind);
+	ind = start;
+	//	System.out.println("after initial swap: "+ Arrays.toString(data));
 	while (i <= j) {
-	    if (data[i] >= pivot && i != ind) {
+	    if (data[i] > pivot) {
 		swap(data,i,j);
 		j--;
+	    }
+	    else if (data[i] < pivot) {
+		i++;
 	    }
 	    else {
 		i++;
 	    }
-	    System.out.println("i, j: " +i+","+j);
-	    System.out.println(Arrays.toString(data));
+	    //  System.out.println("i, j: " +i+","+j);
+	    //System.out.println(Arrays.toString(data));
 	}
 	swap(data,ind,j);
 	//swap(data,last,last-1);
@@ -37,36 +40,27 @@ public class Quick {
 	Random rand = new Random();
 	int ind = start + rand.nextInt(end-start);
 	int pivot = data[ind];
-	System.out.println("pivot: " + ind + "," + pivot);
-	int i = 1;
+	int i = start+1;
 	int lt = start;
 	int gt = end;
 	swap(data,ind,start);
 	while (i <= gt) {
-	    //System.out.println("Before: " + Arrays.toString(data));
 	    if (data[i] == pivot) {
-		if (i < gt) i++;
+		i++;
 	    }
 	    else if (data[i] < pivot) {
-		//System.out.println(i+","+data[i]);
-		//System.out.println(data[i] <= pivot);
 		swap(data,i,lt);
 		i++;
 		lt++;
-		//System.out.println("After: " + Arrays.toString(data));
 	    }
 	    else {
 		swap(data,i,gt);
 		gt--;
 	    }
-	    //System.out.println("i,ind,pivot,left,right: " + i + "," + ind + "," + pivot + "," + lt + "," + gt);
 	}
-	//swap(data,ind,gt);
 	int[] ans = new int[2];
 	ans[0] = lt;
 	ans[1] = gt;
-	//	System.out.println("partition on pivot " +pivot+","+i+": " +Arrays.toString(data));
-	//	System.out.println("lt,gt: " + Arrays.toString(ans));
 	return ans;
     }
 
@@ -74,43 +68,82 @@ public class Quick {
 	quickSortHelp(ary,0,ary.length-1);
     }
 
-    private static void quickSortHelp(int[] arr, int left, int right) {
-	System.out.println(left+","+right);
-	if (left < right) {
-	    System.out.println("left,right: " + left + "," + right);
-	    int[] p = part(arr,left,right);
-	    System.out.println("postpartum: " + Arrays.toString(arr));
-	    quickSortHelp(arr,0,p[1]);
-	    quickSortHelp(arr,p[1]+1,arr.length-1);
+    private static void quickSortHelp(int[] data, int start, int end) {
+	if (end > start) {
+	    Random rand = new Random();
+	    int ind = start + rand.nextInt(end-start);
+	    int pivot = data[ind];
+	    int i = start+1;
+	    int lt = start;
+	    int gt = end;
+	    swap(data,ind,start);
+	    while (i <= gt) {
+		if (data[i] == pivot) {
+		    i++;
+		}
+		else if (data[i] < pivot) {
+		    swap(data,i,lt);
+		    i++;
+		    lt++;
+		}
+		else {
+		    swap(data,i,gt);
+		    gt--;
+		}
+	    }
+	    quickSortHelp(data,start,lt);
+	    quickSortHelp(data,gt+1,end);
 	}
     }
 
     //return the value that is the kth smallest value of the array
-    /*public static int quickselect(int[] data, int k){
-	//System.out.println("before: "+Arrays.toString(data));
-	int[] temp = new int[1];
-	int part = part(data,0,data.length-1);
-        if (k == part) return data[part];
-	if (k > part) {
-	    
-	}
-	else if (k < part) {
-	    
-	}
-        if (Arrays.equals(data,temp)) return data[data.length-1];
-        //System.out.println("after: " + Arrays.toString(temp));
-	}*/
+    public static int quickselect(int[] data, int k) {
+        return quickSelectHelp(data,k,0,data.length-1);
+    }
 
-    
-    
-    public static void main(String[] args) {
+    private static int quickSelectHelp(int[] data, int k, int start, int end) {
+	if (end > start) {
+	    Random rand = new Random();
+	    int ind = start + rand.nextInt(end-start);
+	    int pivot = data[ind];
+	    int i = start+1;
+	    int lt = start;
+	    int gt = end;
+	    swap(data,ind,start);
+	    while (i <= gt) {
+		if (data[i] == pivot) {
+		    i++;
+		}
+		else if (data[i] < pivot) {
+		    swap(data,i,lt);
+		    i++;
+		    lt++;
+		}
+		else {
+		    swap(data,i,gt);
+		    gt--;
+		}
+	    }
+	    if (k < lt) return quickSelectHelp(data,k,start,lt);
+	    if (k > gt) return quickSelectHelp(data,k,gt+1,end);
+	}
+	return data[k];
+    }
+        
+    /*public static void main(String[] args) {
 	Quick a = new Quick();
-	//int[] test1 = {3,1,4,3,5,3,2,6,5,3};
-	int[] test1 = {2,10,15,23,0,5};
-	//	int[] test = {44,75,23,43,55,12,64,77,33};
-       	System.out.println(Arrays.toString(test1));
-	quicksort(test1);
+	//int[] test1 = {1,3,4,3,7,3,0,3,8,3,2,3};
+	//int[] test1 = {3,1,4,9,12,10,2,6,5,7};
+	int[] test1 = new int[5000];
+	for (int i = 0; i < test1.length; i++)
+	    test1[i] = (int)(Math.random()*test1.length);
+	//int[] test1 = {2,10,15,23,0,5};
+	//int[] test = Arrays.copyOfRange(test1,0,test1.length);
+	//System.out.println(Arrays.toString(test1));
+	//quicksort(test1);
 	System.out.println(Arrays.toString(test1));
+	//int[] t = {2,10,5,15,0,23};//ans: {1,3,2,4,0,5}
+	for (int i = 0; i < test1.length; i++) System.out.println(quickselect(test1,i));
 	//System.out.println(Arrays.toString(test1));
 	//part(test1,0,test1.length-1);
 	//System.out.println(Arrays.toString(test1));
@@ -118,5 +151,5 @@ public class Quick {
 	//  System.out.println(k + ": " + quickselect(test1,k));
 	//}
 	//System.out.println(Arrays.toString(test1));
-    }
+	}*/
 }
