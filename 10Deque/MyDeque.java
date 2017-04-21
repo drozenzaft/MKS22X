e78c6391d7e6b2c6e533bafe2981bfec2f0cff68
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-04-21 14:47:34 -0400

    Additions

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,3 +1,26 @@
-public class MyDeque {
-
+public interface MyDeque extends Queue {
+  private int front;
+  private int back;
+  private int size;
+  private String[] deque;
+  
+  public MyDeque() {
+    size = 10;
+    front = 0;
+    back = 0;
+    deque = new String[size];
+  }
+  
+  private void nPE(String s) {
+    try {
+      int a = s.length();
+    }
+    catch (NullPointerException e) {
+      throw new NullPointerException("Specified element is null: " + s);
+    }
+  }
+  
+  public void addFirst(String s) {
+    nPE(s);
+    
 }
