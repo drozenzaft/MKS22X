8e587f43d8db822029a0f8569c1dea1255b0a65a
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-05-08 23:22:06 -0400

    I still can't fix this max problem

---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,53 @@
+public class RunningMedian {
+    private MyHeap min;
+    private MyHeap max;
+    public RunningMedian() {
+	min = new MyHeap();
+	max = new MyHeap(false);
+    }
+    public double getMedian() {
+	if ((max.size() + min.size()) % 2 == 0)
+	    return (max.peek() + min.peek()) / 2.0;
+	return oddMedian();
+    }
+    public void add(int value) {
+	if (max.size() >= 1 && min.size() >= 1) System.out.println("before add: " + max + "," + min);
+	if (value >= getMedian()) {
+	    max.add(value);
+	}
+	else {
+	    min.add(value);
+	}	
+	if (max.size() - min.size() > 1) {
+	    min.add(max.remove());
+	}
+	else if (min.size() - max.size() > 1) {
+	    max.add(min.remove());
+	}
+	if (max.size() >= 1 && min.size() >= 1) System.out.println("after add: " + max + "," + min);
+    }
+    private int oddMedian() {
+	if (min.size() > max.size()) return min.peek();
+	else return max.peek();
+    }
+    public static void main(String[] args) {
+	RunningMedian rm = new RunningMedian();
+	rm.add(1);
+	rm.add(64);
+	rm.add(20);
+	rm.add(34);
+	rm.add(44);
+	rm.add(49);
+	rm.add(56);
+	rm.add(401);
+	rm.add(714);
+	rm.add(1021);
+	/*rm.add(7814);
+	rm.add(9626);
+	rm.add(10551);*/
+	//rm.add(56);
+	//rm.add(12717);
+	System.out.println(rm.max + ", " + rm.min);
+	System.out.println(rm.getMedian());
+    }
+}
