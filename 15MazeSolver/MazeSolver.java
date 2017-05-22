5b4700fb82c856f2ccdc2740e0c9097a5bd3b557
Daniel Rozenzaft <drozenzaft@stuy.edu>
2017-05-22 01:53:49 -0400

    I wasn't able to get the MazeSolver. I'll talk to you in class (or during 3rd period) tomorrow, 5/22, about getting help (and an extension). (PLEASE DON'T MARK THIS LATE, THANKS!!!)

--- 15MazeSolver/MazeSolver.java
+++ 15MazeSolver/MazeSolver.java
@@ -1,3 +1,41 @@
+import java.util.*;
+import java.io.*;
 public class MazeSolver {
-
+    private MyHeap frontier;
+    private char[][] maze;
+    public MazeSolver(String filename) {
+	this(filename,false);
+    }
+    public MazeSolver(String filename, boolean animate) {
+        maze = new Maze(filename);
+	//place holder for animate...
+    }
+    public void solve() {
+	solve(1);
+    }
+    public void solve(int style) {
+	switch (style) {
+	    case 0:
+		//DFS
+	    case 1:
+		//BFS
+		break;
+	    case 2:
+		//BestFirst
+		break;
+	    case 3:
+		//A*
+		break;
+	}
+	Location start = maze.getStart();
+	Location current = frontier.peek();
+	while (!current.equals(maze.getEnd()) && frontier.size() > 0) {
+	    frontier.remove();
+	    current = frontier.peek();
+	}
+    }
+    public String toString() {
+	return maze.toString();
+    }
+    
 }
